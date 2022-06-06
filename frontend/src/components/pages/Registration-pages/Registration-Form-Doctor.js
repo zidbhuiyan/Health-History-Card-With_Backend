1a8d5648d67c3fbc50a83754a159e9583785e0aa
shakila-miti <100066423+shakila-miti@users.noreply.github.com>
2022-06-06 13:32:46 +0600

    Update Registration-Form-Doctor.js

--- frontend/src/components/pages/Registration-pages/Registration-Form-Doctor.js
+++ frontend/src/components/pages/Registration-pages/Registration-Form-Doctor.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../../../App';
 import Footer from '../../Footer';
 import { Link } from 'react-router-dom';
@@ -7,6 +8,61 @@ import './Registration-Form.css'
 
 
 function Registration_Form_Doctor() {
+  
+  const [user, setUser] = useState({
+    Dfirstname: '',
+    Dlastname: '',
+    Docregid: '',
+    Dnid: '',
+    Ddateofbirth: '',
+    Dphonenumber: '',
+    hospitalname: '',
+    Dgender: '',
+    Dpassword: '',
+    Dconfirmpassword: '',
+    })
+
+    function handleChange (e) {
+      const {name, value} = e.target;
+      setUser(prevUser => {
+        return{
+        ...prevUser,
+        [name]: value 
+      }
+      })
+    }
+
+    function handleClick(event) {
+      event.preventDefault();
+  
+      const newDoctor = {
+      Dfirstname: user.Dfirstname,
+      Dlastname: user.Dlastname,
+      Docregid: user.Docregid,
+      Dnid: user.Dnid,
+      Ddateofbirth: user.Ddateofbirth,
+      Dphonenumber: user.Dphonenumber,
+      hospitalname: user.hospitalname,
+      Dgender: user.Dgender,
+      Dpassword: user.Dpassword,
+      Dconfirmpassword: user.Dconfirmpassword,
+      }
+  
+      if(user.Dfirstname && user.Dlastname && user.Docregid && user.Dnid && user.Ddateofbirth && user.Dphonenumber && user.hospitalname && user.Dgender && user.Dpassword && user.Dconfirmpassword ){
+        alert("Successfully Registered!")
+        axios.post('http://localhost:3001/createDoc', newDoctor)
+        .then( res => console.log(res))
+      }
+  
+      else{
+        alert("Fill up all the fields!")
+      }
+      
+     
+    }
+
+
+
   return (
     <>
     <div className='bgg'>
@@ -21,35 +77,35 @@ function Registration_Form_Doctor() {
         <div class="user-details">
           <div class="input-box">
             <span class="details">First Name</span>
-            <input type="text" placeholder="Enter your First name" required/>
+            <input type="text" name="Dfirstname" value={user.Dfirstname} placeholder="Enter your First name" id="Dfirstname" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Last name</span>
-            <input type="text" placeholder="Enter your Last name" required/>
+            <input type="text" name="Dlastname" value={user.Dlastname}  placeholder="Enter your Last name" id="Dlastname" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Doctor Register ID</span>
-            <input type="text" placeholder="Enter your Register ID" required/>
+            <input type="text" name="Docregid" value={user.Docregid} placeholder="Enter your Register ID" id="Docregid" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">NID</span>
-            <input type="text" placeholder="Enter your NID" required/>
+            <input type="text" name="Dnid" value={user.Dnid}  placeholder="Enter your NID" id="Dnid" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Date of Birth(MM/DD/YYYY)</span>
-            <input type="date" placeholder="Enter your Date of Birth" required/>
+            <input type="date" name="Ddateofbirth" value={user.Ddateofbirth}  placeholder="Enter your Date of Birth" id="Ddateofbirth" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Phone Number</span>
-            <input type="text" placeholder="Enter your number" required/>
+            <input type="text" name="Dphonenumber" value={user.Dphonenumber}  placeholder="Enter your number" id="Dphonenumber" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Hospital Name</span>
-            <input type="text" placeholder="Enter hospital name" required/>
+            <input type="text" name="hospitalname" value={user.hospitalname} placeholder="Enter hospital name" id="hospitalname" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Gender</span>
-            <select name="gender">
+            <select name="Dgender" value={user.Dgender} id="Dgender" onChange={ handleChange }>
             <option value="" disabled selected>Select Gender</option>
 	          <option value="male">Male</option>
 	          <option value="female">Female</option>
@@ -58,17 +114,17 @@ function Registration_Form_Doctor() {
           </div>
           <div class="input-box">
             <span class="details">Password</span>
-            <input type="text" placeholder="Enter your password" required/>
+            <input type="password" name="Dpassword" value={user.Dpassword} placeholder="Enter your password" id="Dpassword" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required/>
+            <input type="password" name="Dconfirmpassword" value={user.Dconfirmpassword} placeholder="Confirm your password" id="Dconfirmpassword" onChange={ handleChange } required/>
           </div>
           
         </div>
        
         <div class="button">
-          <input type="submit" value="Register"/>
+        <input onClick={handleClick} type="submit" value="Register" id="submitBtn"/>
         </div>
       </form>
     </div>
