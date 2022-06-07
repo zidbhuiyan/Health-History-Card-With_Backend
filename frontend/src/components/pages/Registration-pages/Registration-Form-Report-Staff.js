833c298c6661bf012394b460687b739bee126eb1
shakila-miti <100066423+shakila-miti@users.noreply.github.com>
2022-06-07 07:54:41 +0600

    Update Registration-Form-Report-Staff.js

--- frontend/src/components/pages/Registration-pages/Registration-Form-Report-Staff.js
+++ frontend/src/components/pages/Registration-pages/Registration-Form-Report-Staff.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import '../../../App';
 import Footer from '../../Footer';
 import { Link } from 'react-router-dom';
@@ -7,6 +8,60 @@ import './Registration-Form.css'
 
 
 function Registration_Form_Report_Staff() {
+  
+  const [user, setUser] = useState({
+    firstname: '',
+    lastname: '',
+    Rregid: '',
+    nid: '',
+    dateofbirth: '',
+    phonenumber: '',
+    hospitalname: '',
+    gender: '',
+    password: '',
+    confirmpassword: '',
+    })
+
+  function handleChange (e) {
+    const {name, value} = e.target;
+    setUser(prevUser => {
+      return{
+      ...prevUser,
+      [name]: value 
+    }
+    })
+    
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+
+    const newReportstaff = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+      Rregid: user.Rregid,
+      nid: user.nid,
+      dateofbirth: user.dateofbirth,
+      phonenumber: user.phonenumber,
+      hospitalname: user.hospitalname,
+      gender: user.gender,
+      password: user.password,
+      confirmpassword: user.confirmpassword,
+    }
+
+    if(user.firstname && user.lastname && user.Rregid && user.nid && user.dateofbirth && user.phonenumber && user.hospitalname && user.gender && user.password && user.confirmpassword  ){
+      alert("Successfully Registered!")
+      axios.post('http://localhost:3001/create3', newReportstaff)
+      .then( res => console.log(res))
+    }
+
+    else{
+      alert("Fill up all the fields!")
+    }
+    
+   
+  }
+  
   return (
     <>
     <div className='bgg'>
@@ -21,36 +76,36 @@ function Registration_Form_Report_Staff() {
         <div class="user-details">
           <div class="input-box">
             <span class="details">First Name</span>
-            <input type="text" placeholder="Enter your First name" required/>
+            <input type="text" name="firstname" value={user.firstname} placeholder="Enter your First name" id="firstname" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Last name</span>
-            <input type="text" placeholder="Enter your Last name" required/>
+            <input type="text" name="lastname" value={user.lastname}  placeholder="Enter your Last name" id="lastname" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Report Staff Register ID</span>
-            <input type="text" placeholder="Enter your Register ID" required/>
+            <input type="text" name="Rregid" value={user.Rregid} placeholder="Enter your Register ID" id="Rregid" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">NID</span>
-            <input type="text" placeholder="Enter your NID" required/>
+            <input type="text" name="nid" value={user.nid}  placeholder="Enter your NID" id="nid" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Date of Birth(MM/DD/YYYY)</span>
-            <input type="date" placeholder="Enter your Date of Birth" required/>
+            <input type="date" name="dateofbirth" value={user.dateofbirth}  placeholder="Enter your Date of Birth" id="dateofbirth" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Phone Number</span>
-            <input type="text" placeholder="Enter your number" required/>
+            <input type="text" name="phonenumber" value={user.phonenumber}  placeholder="Enter your number" id="phonenumber" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Hospital Name</span>
-            <input type="text" placeholder="Enter hospital name" required/>
+            <input type="text" name="hospitalname" value={user.hospitalname} placeholder="Enter hospital name" id= "hospitalname" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Gender</span>
-            <select name="gender">
-            <option value="" disabled selected>Select Gender</option>
+            <select name="gender" value={user.gender} id="gender" onChange={ handleChange }>
+            <option value="" disabled selected>Select Gender </option>
 	          <option value="male">Male</option>
 	          <option value="female">Female</option>
           	<option value="other">Prefer not to say</option>
@@ -58,17 +113,17 @@ function Registration_Form_Report_Staff() {
           </div>
           <div class="input-box">
             <span class="details">Password</span>
-            <input type="text" placeholder="Enter your password" required/>
+            <input type="password" name="password" value={user.password} placeholder="Enter your password" id="password" onChange={ handleChange } required/>
           </div>
           <div class="input-box">
             <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" required/>
+            <input type="password" name="confirmpassword" value={user.confirmpassword} placeholder="Confirm your password" id="confirmpassword" onChange={ handleChange } required/>
           </div>
           
         </div>
         
         <div class="button">
-          <input type="submit" value="Register"/>
+        <input onClick={handleClick} type="submit" value="Register" id="submitBtn"/>
         </div>
       </form>
     </div>
