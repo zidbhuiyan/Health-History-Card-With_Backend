0c7dd3bcdedef4fc9c82396c87285087d77c99cb
John Doe <johndoe@email.com>
2022-06-10 09:24:45 +0600

    login form updated

--- frontend/src/components/pages/Login-pages/Login-Form-Doctor.js
+++ frontend/src/components/pages/Login-pages/Login-Form-Doctor.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../App';
+import { Link } from 'react-router-dom';
 import Footer from '../../Footer';
 import Navbar from '../../Navbar';
 import './Login-Form.css'
-import Login_Form from './LoginForm';
-
 
 function Login_Form_Doctor() {
+  const [user, setUser] = useState({
+    birthid: "",
+    nid: "",
+    password: "",
+    })
+  
+  const handleChange = e =>{
+    const {name, value} = e.target
+    setUser({
+      ...user,
+      [name]: value 
+    })
+    
+  }
   return (
     <>
+      <div className='bgg'>
+    <Navbar
+      NavPage="other"/>
 
-      <Login_Form 
-        cat = 'Doctor'
-        catid = 'Doctor Register'/>
-     
+    <div className="center">
+      
+      <h1>Login As Doctor</h1>
+      <form>
+        <div className="txt_field">
+          <input type="text" name="birthid" value={user.birthid} onChange={handleChange} required/>
+          <span></span>
+          <label>Enter Registration ID</label>
+        </div>
+        <div className="txt_field">
+          <input type="password" name="password" value={user.password} onChange={handleChange} required/>
+          <span></span>
+          <label>Enter Password</label>
+        </div>
+        
+        <input type="submit" value="Login"/>
+        <div className="signup_link">
+        Not Registered? <Link to='/registration'>Register Here</Link>
+        </div>
+      </form>
+    </div>
+    <Footer/>
+    </div>
+ 
     </>
   );
 }
--- frontend/src/components/pages/Login-pages/Login-Form-Vaccine-Staff.js
+++ frontend/src/components/pages/Login-pages/Login-Form-Vaccine-Staff.js
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../../App';
+import { Link } from 'react-router-dom';
+import Footer from '../../Footer';
+import Navbar from '../../Navbar';
 import './Login-Form.css'
-import Login_Form from './LoginForm';
-
 
 function Login_Form_Vaccine_Staff() {
+  const [user, setUser] = useState({
+    birthid: "",
+    nid: "",
+    password: "",
+    })
+  
+  const handleChange = e =>{
+    const {name, value} = e.target
+    setUser({
+      ...user,
+      [name]: value 
+    })
+    
+  }
   return (
     <>
+      <div className='bgg'>
+    <Navbar
+      NavPage="other"/>
+
+    <div className="center">
       
-      <Login_Form 
-        cat = 'Vaccine Staff'
-        catid = 'Vaccine Staff Registration'/>
-     
+      <h1>Login As Vaccine Staff</h1>
+      <form>
+        <div className="txt_field">
+          <input type="text" name="birthid" value={user.birthid} onChange={handleChange} required/>
+          <span></span>
+          <label>Enter Registration ID</label>
+        </div>
+        <div className="txt_field">
+          <input type="password" name="password" value={user.password} onChange={handleChange} required/>
+          <span></span>
+          <label>Enter Password</label>
+        </div>
+        
+        <input type="submit" value="Login"/>
+        <div className="signup_link">
+        Not Registered? <Link to='/registration'>Register Here</Link>
+        </div>
+      </form>
+    </div>
+    <Footer/>
+    </div>
+ 
     </>
   );
 }
